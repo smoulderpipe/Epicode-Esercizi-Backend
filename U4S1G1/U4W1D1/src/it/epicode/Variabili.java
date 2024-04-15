e9cf91d2ddc066d60a073047bfa08bc8cfe82bf7
smoulderpipe <del.v.paola@gmail.com>
2024-04-15 14:35:54 +0200

    test: U4S1G1 - Prima commit da Intellij

---
+++ U4S1G1/U4W1D1/src/it/epicode/Variabili.java
@@ -0,0 +1,29 @@
+package it.epicode;
+
+import java.util.Scanner;
+
+public class Variabili {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int g = scanner.nextInt();
+        int x;
+        x = 4;
+        x = 6;
+
+        boolean b = true;
+
+        char c = 'c';
+
+        double d = 3.3;
+        String s = "benvenuti al corso epicode";
+        //String x = "ciaociao";
+        //String fraseConcatenata = s + x;
+        System.out.println(s);
+
+        int[] numeri = new int[6];
+    }
+
+
+}
