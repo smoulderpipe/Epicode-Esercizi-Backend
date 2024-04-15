bfb13b10bf7f869fb491438753eea9f205e4edc6
smoulderpipe <del.v.paola@gmail.com>
2024-04-15 16:54:57 +0200

    feat: U4S1G1 - Primo esercizio Java

--- U4S1G1/U4W1D1/src/Main.java
+++ U4S1G1/U4W1D1/src/Main.java
@@ -1,15 +1,64 @@
+import it.epicode.Variabili;
+import java.util.Scanner;
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        // Crea un'istanza della classe Variabili
+        Variabili variabili = new Variabili();
+
+        // Chiama i metodi della classe Variabili
+        int risultatoMoltiplicazione = variabili.moltiplica(10, 5);
+        String risultatoConcatenazione = variabili.concatena("ciao", 8);
+        String[] nuovoArray = variabili.inserisciInArray(new String[]{"lunedì", "martedì", "mercoledì", "giovedì", "venerdì"}, "sabato");
+        String[] treStringhe = Variabili.inserisciTreStringhe();
+
+        // Stampa i risultati
+        System.out.println("Risultato moltiplicazione: " + risultatoMoltiplicazione);
+        System.out.println("Risultato concatenazione: " + risultatoConcatenazione);
+        System.out.println("Risultato inserisciInArray: ");
+        for (String elemento : nuovoArray) {
+            System.out.print(elemento + ", ");
+        };
+        String concatenazioneOrdine = treStringhe[0] + " " + treStringhe[1] + " " + treStringhe[2];
+        System.out.println("Concatenazione in ordine: " + concatenazioneOrdine);
+        String concatenazioneInverso = treStringhe[2] + " " + treStringhe[1] + " " + treStringhe[0];
+        System.out.println("Concatenazione in ordine inverso: " + concatenazioneInverso);
+
+        System.out.print("Inserisci la base del rettangolo: ");
+        double baseRettangolo = scanner.nextDouble();
+
+        System.out.print("Inserisci l'altezza del rettangolo: ");
+        double altezzaRettangolo = scanner.nextDouble();
+
+        double perimetroRettangolo = Variabili.perimetroRettangolo(baseRettangolo, altezzaRettangolo);
+        System.out.println("Perimetro del rettangolo: " + perimetroRettangolo);
+
+        System.out.print("Inserisci un numero intero: ");
+        int numero = scanner.nextInt();
+
+        int risultatoPariDispari = Variabili.pariDispari(numero);
+        if (risultatoPariDispari == 0) {
+            System.out.println(numero + " è pari");
+        } else {
+            System.out.println(numero + " è dispari");
         }
+
+        System.out.print("Inserisci il lato A del triangolo: ");
+        double latoATriangolo = scanner.nextDouble();
+
+        System.out.print("Inserisci il lato B del triangolo: ");
+        double latoBTriangolo = scanner.nextDouble();
+
+        System.out.print("Inserisci il lato C del triangolo: ");
+        double latoCTriangolo = scanner.nextDouble();
+
+        double areaTriangolo = Variabili.perimetroTriangolo(latoATriangolo, latoBTriangolo, latoCTriangolo);
+        System.out.println("Area del triangolo: " + areaTriangolo);
     }
 }
--- U4S1G1/U4W1D1/src/it/epicode/Variabili.java
+++ U4S1G1/U4W1D1/src/it/epicode/Variabili.java
@@ -1,29 +1,93 @@
+/*
+Esercizio 1
+Implementare i seguenti metodi:
+- moltiplica: accetta due interi e ritorna il loro prodotto
+- concatena: accetta una stringa e un intero e restituisce una stringa che concatena gli elementi
+- inserisciInArray: accetta un array di stringhe di cinque elementi ed una stringa e restituisce un array di sei elementi, in cui la stringa passata sia al terzo posto e le stringhe precedentemente in quarta e quinta posizione siano rispettivamente in quinta e sesta.
+
+Scrivi una main che invochi in sequenza i tre metodi realizzati
+ */
+
+
 package it.epicode;
 
 import java.util.Scanner;
 
 public class Variabili {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int g = scanner.nextInt();
-        int x;
-        x = 4;
-        x = 6;
-
-        boolean b = true;
-
-        char c = 'c';
-
-        double d = 3.3;
-        String s = "benvenuti al corso epicode";
-        //String x = "ciaociao";
-        //String fraseConcatenata = s + x;
-        System.out.println(s);
-
-        int[] numeri = new int[6];
+    public int moltiplica(int x, int y) {
+        return x * y;
     }
 
+    public String concatena(String s, int y) {
+        return s + String.valueOf(y);
+    }
+
+    public String[] inserisciInArray(String[] array, String r) {
+        String[] nuovoArray = new String[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            nuovoArray[i] = array[i];
+        }
+        nuovoArray[3] = array[2];
+        nuovoArray[5] = array[4];
+        nuovoArray[4] = array[3];
+        nuovoArray[2] = r;
+
+        return nuovoArray;
+    }
+
+    public static String[] inserisciTreStringhe() {
+        Scanner scanner = new Scanner(System.in); // Crea lo Scanner all'interno del metodo
+
+        System.out.print("Inserisci la prima stringa: ");
+        String stringaUno = scanner.nextLine();
+
+        System.out.print("Inserisci la seconda stringa: ");
+        String stringaDue = scanner.nextLine();
+
+        System.out.print("Inserisci la terza stringa: ");
+        String stringaTre = scanner.nextLine();
+
+        String[] newArray = new String[]{stringaUno, stringaDue, stringaTre};
+        return newArray;
+    }
+
+    public static double perimetroRettangolo(double base, double altezza) {
+        return 2 * (base + altezza);
+    }
+
+    public static int pariDispari(int numero) {
+        if (numero % 2 == 0) {
+            return 0; // Pari
+        } else {
+            return 1; // Dispari
+        }
+    }
+
+    public static double perimetroTriangolo(double latoA, double latoB, double latoC) {
+        double semiperimetro = (latoA + latoB + latoC) / 2;
+        double area = Math.sqrt(semiperimetro * (semiperimetro - latoA) * (semiperimetro - latoB) * (semiperimetro - latoC));
+        return area;
+    }
 
 }
+
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int g = scanner.nextInt();
+//        int x;
+//        x = 4;
+//        x = 6;
+//
+//        boolean b = true;
+//
+//        char c = 'c';
+//
+//        double d = 3.3;
+//        String s = "benvenuti al corso epicode";
+//        //String x = "ciaociao";
+//        //String fraseConcatenata = s + x;
+//        System.out.println(s);
+//
+//        int[] numeri = new int[6];
